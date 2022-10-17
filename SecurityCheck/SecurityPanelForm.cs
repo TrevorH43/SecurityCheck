46e0e0280a56b19b7e52e8fac097f0c3ab90bccc
Trevor Hattendorf <Trevor.hattendorf@gmail.com>
2022-10-17 10:53:01 -0500

    Add project files.

---
+++ SecurityCheck/SecurityPanelForm.cs
@@ -0,0 +1,15 @@
+namespace SecurityCheck
+{
+    public partial class SecurityPanelForm : Form
+    {
+        public SecurityPanelForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SecurityPanelForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
